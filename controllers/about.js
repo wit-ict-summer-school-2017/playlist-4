dcc95f9fe9ffe61e49f3f09e4d3accdaf02538ba
Eamonn de Leastar <edeleastar@wit.ie>
2017-05-16 09:16:35 +0100

    branding to playlist 1

--- controllers/about.js
+++ controllers/about.js
@@ -6,7 +6,7 @@ const about = {
   index(request, response) {
     logger.info('about rendering');
     const viewData = {
-      title: 'About Template 1',
+      title: 'About Playlist 1',
     };
     response.render('about', viewData);
   },
