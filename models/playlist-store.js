9138cfd4d6121ed692779047263b58e22d87e7ef
Eamonn de Leastar <edeleastar@wit.ie>
2017-05-16 10:15:55 +0100

    move the playlist object to an external json file

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -1,43 +1,5 @@
 'use strict';
 
-const logger = require('../utils/logger');
-
-const sonatas = {
-  title: 'Beethoven Sonatas',
-  songs: [
-    {
-      title: 'Piano Sonata No. 3',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 7',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 10',
-      artist: 'Beethoven',
-    },
-  ],
-};
-
-const concertos = {
-  title: 'Beethoven Concertos',
-  songs: [
-    {
-      title: 'Piano Concerto No. 0',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Concerto  No. 4',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Concerto  No. 6',
-      artist: 'Beethoven',
-    },
-  ],
-};
-
-const playlistCollection = [sonatas, concertos];
+const playlistCollection = require('./playlist-store.json').playlistCollection;
 
 module.exports = playlistCollection;
