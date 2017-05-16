7e78998dd45bef358fa2faf7c7a8526a0b4c689d
Eamonn de Leastar <edeleastar@wit.ie>
2017-05-16 09:56:00 +0100

    move sonatas playlist object to a playlist-store.js module

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,30 +1,13 @@
 'use strict';
 
 const logger = require('../utils/logger');
-
-const sonatas = {
-  title: 'Sonatas',
-  songs: [
-    {
-      title: 'Piano Sonata No. 3',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 7',
-      artist: 'Beethoven',
-    },
-    {
-      title: 'Piano Sonata No. 10',
-      artist: 'Beethoven',
-    },
-  ],
-};
+const sonatas = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
     const viewData = {
-      title: 'Playlist 1 Dashboard',
+      title: 'Playlist Dashboard',
       playlist: sonatas,
     };
     logger.info('about to render', sonatas);
---
+++ models/playlist-store.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const logger = require('../utils/logger');
+
+const sonatas = {
+  title: 'Beethoven Sonatas',
+  songs: [
+    {
+      title: 'Piano Sonata No. 3',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 7',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 10',
+      artist: 'Beethoven',
+    },
+  ],
+};
+
+module.exports = sonatas;
