ab7ea8d024fdafb3a2d125b57ba937306f5405fc
Eamonn de Leastar <edeleastar@wit.ie>
2017-06-12 07:41:26 +0100

    first attempt at playlist controller

---
+++ controllers/playlist.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const logger = require('../utils/logger');
+const playlistCollection = require('../models/playlist-store.js');
+
+const playlist = {
+  index(request, response) {
+    const viewData = {
+      title: 'Playlist',
+    };
+    response.render('playlist', viewData);
+  },
+};
+
+module.exports = playlist;
