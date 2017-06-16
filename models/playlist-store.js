01b77992d160fffe8d6728ca68f929cfd66e6eef
Eamonn de Leastar <edeleastar@wit.ie>
2017-06-16 07:26:36 +0100

    json persistence introduced

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -1,35 +1,48 @@
 'use strict';
 
 const _ = require('lodash');
+const JsonStore = require('./json-store');
 
 const playlistStore = {
 
-  playlistCollection: require('./playlist-store.json').playlistCollection,
+  store: new JsonStore('./models/playlist-store.json', { playlistCollection: [] }),
+  collection: 'playlistCollection',
 
   getAllPlaylists() {
-    return this.playlistCollection;
+    return this.store.findAll(this.collection);
   },
 
   getPlaylist(id) {
-    return _.find(this.playlistCollection, { id: id });
+    return this.store.findOneBy(this.collection, { id: id });
   },
 
   addPlaylist(playlist) {
-    this.playlistCollection.push(playlist);
-  },
-
-    removeSong(id, songId) {
-    const playlist = this.getPlaylist(id);
-    _.remove(playlist.songs, { id: songId });
+    this.store.add(this.collection, playlist);
+    this.store.save();
   },
 
   removePlaylist(id) {
-    _.remove(this.playlistCollection, { id: id });
+    const playlist = this.getPlaylist(id);
+    this.store.remove(this.collection, playlist);
+    this.store.save();
+  },
+
+  removeAllPlaylists() {
+    this.store.removeAll(this.collection);
+    this.store.save();
   },
 
   addSong(id, song) {
     const playlist = this.getPlaylist(id);
     playlist.songs.push(song);
+    this.store.save();
+  },
+
+  removeSong(id, songId) {
+    const playlist = this.getPlaylist(id);
+    const songs = playlist.songs;
+    _.remove(songs, { id: songId});
+    this.store.save();
   },
 };
 
