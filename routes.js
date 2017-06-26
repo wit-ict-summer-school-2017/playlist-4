a08ad7f7c2f903e2918113fefc1e58b4206ac3bc
Eamonn de Leastar <edeleastar@wit.ie>
2017-06-26 09:38:35 +0100

    account management routes

--- routes.js
+++ routes.js
@@ -3,11 +3,18 @@
 const express = require('express');
 const router = express.Router();
 
+const accounts = require('./controllers/accounts.js');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
 const playlist = require('./controllers/playlist.js');
 
-router.get('/', dashboard.index);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
 router.get('/dashboard', dashboard.index);
 router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 router.post('/dashboard/addplaylist', dashboard.addPlaylist);
